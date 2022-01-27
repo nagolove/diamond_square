d10a3eb2f554dea49bf33ed5fa601a4aaeee84bb
nagolove <dekar.own@gmail.com>
2022-01-27 21:14:00 +0300

    generated file.

--- init.lua
+++ init.lua
@@ -62,6 +62,7 @@ local vec2 = require("vector")
 local vecl = require("vector-light")
 local tabular = require("tabular")
 local pw = require("physics_wrapper")
+local Cm = require('chipmunk')
 
 
 local pipeline = Pipeline.new(SCENE_PREFIX)
@@ -77,9 +78,6 @@ local Shortcut = KeyConfig.Shortcut
 
 
 
-
-
-local joyState
 
 local Mode = {}
 
@@ -203,7 +201,14 @@ local Hangar = {}
 
 
 
-local Tank = {}
+local Tank = {Options = {}, }
+
+
+
+
+
+
+
 
 
 
@@ -458,6 +463,9 @@ local Hit = {}
 
 
 
+local bodyIter
+local shapeIter
+
 
 maxParticlesNumber = 512
 
@@ -509,6 +517,7 @@ require("Timer")
 
 
 local camTimer = require("Timer").new()
+local camera = love.math.newTransform()
 
 
 showLogo = true
@@ -562,17 +571,7 @@ require('diamondsquare')
 
 
 
-
-
-
-
-
-
 local drawTerrain = true
-
-
-
-
 
 maxTrackCount = 128
 hits = {}
@@ -585,9 +584,8 @@ local is_stop = false
 local last_render = love.timer.getTime()
 
 
-
-
 local joystick = love.joystick
+local joyState
 
 local joy
 
@@ -602,29 +600,6 @@ local function initJoy()
    end
    joyState = JoyState.new(joy)
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -679,6 +654,43 @@ function Bullet.new(px, py, dirx, diry,
 
    return self
 
+end
+
+local function gather_verts(shape)
+   local num = pw.polyShapeGetCount(shape)
+   local verts = {}
+   for i = 0, num - 1 do
+      local vert = pw.polyShapeGetVert(shape, i)
+      table.insert(verts, vert.x)
+      table.insert(verts, vert.y)
+   end
+   return verts
+end
+
+local function eachShape(b, shape)
+   local shape_type = pw.polyShapeGetType(shape)
+   if shape_type == pw.CP_POLY_SHAPE then
+
+
+
+
+      pipeline:open('poly_shape')
+      local verts = gather_verts(shape)
+      pipeline:push(verts)
+      pipeline:close()
+   end
+end
+
+
+local function eachBody(b)
+   local body = pw.cpBody2Body(b)
+   if body then
+
+
+      pw.eachBodyShape(b, shapeIter)
+   else
+
+   end
 end
 
 
@@ -905,59 +917,6 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function Hit.new(x, y)
    local Hit_mt = {
       __index = Hit,
@@ -1087,30 +1046,32 @@ function Arena.new(fname)
    local Arena_mt = { __index = Arena }
    local self = setmetatable({}, Arena_mt)
 
-   if fname and type(fname) == "string" then
-      local edges = {}
-      local filedata = love.filesystem.read(fname)
-      if filedata then
-         local ok = false
-         local root
-         ok, root = serpent.load(filedata)
-         self.edges = edges
 
-         if (root).rngSeed then
-            local seed = (root).rngSeed
-            print('seed value loaded', seed)
-            rng = love.math.newRandomGenerator(seed)
-         end
 
-         if not ok then
-            print("Could'not do serpent.load()")
-            self:createFixtures()
-         end
-      else
-         self.edges = edges
-         self:createFixtures()
-      end
-   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
    self.objectType = "Arena"
 
@@ -1308,7 +1269,7 @@ local tank_width, tank_height = getTankSize()
 
 
 
-function Tank.new(pos, dir)
+function Tank.new(pos, options)
 
    local Tank_mt = {
       __index = Tank,
@@ -1327,9 +1288,12 @@ function Tank.new(pos, dir)
    self.strength = 1.
    self.fuel = 1.
    self.id = tankCounter
-   if not dir then
-      dir = vector.new(0, 0)
-   end
+   self.first_render = true
+
+
+
+
+
 
 
 
@@ -1345,8 +1309,10 @@ function Tank.new(pos, dir)
 
 
 
-   self.body = pw.newBoxBody(tank_width, tank_height)
+   local body_options = options and options.body_opts
+   self.body = pw.newBoxBody(tank_width, tank_height, body_options)
    self.body:bodySetPosition(pos.x, pos.y)
+   self.body.user_data = self
 
    return self
 
@@ -2130,14 +2096,6 @@ end
 
 
 
-local function terrain(mapn, rez)
-   if not mapn then
-      mapn = 8
-   end
-   if not rez then
-      rez = 128
-   end
-   print('terrain', mapn, rez)
 
 
 
@@ -2145,7 +2103,17 @@ local function terrain(mapn, rez)
 
 
 
-end
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -2182,7 +2150,7 @@ local function bindDeveloperKeys()
          return false, sc
       end
 
-      terrain()
+
 
       return false, sc
    end,
@@ -2437,163 +2405,6 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function reset()
-   print('reset')
-
-   KeyConfig.clear()
-
-
-
-
-
-
-
-
-
-
-   tanks = {}
-   playerTank = nil
-
-
-
-
-
-
-
-
-
-end
-
-local function makeArmy()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-end
-
-
-local makeTank = false
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-local brushFunction
 
 
 
@@ -2932,7 +2743,7 @@ end
 
 
 
-local function render()
+local function renderScene()
 
    local nt = love.timer.getTime()
    local pause = 1. / 400.
@@ -2941,19 +2752,18 @@ local function render()
    if diff >= pause then
       last_render = nt
 
+      pipeline:openAndClose('clear')
 
+      pipeline:open('set_transform')
+      pipeline:push(camera)
+      pipeline:close()
+
+      pw.eachSpaceBody(bodyIter)
+
+      pipeline:openAndClose('pop_transform')
 
       pipeline:sync()
    end
-
-
-
-
-
-
-
-
-
 end
 
 local function updateTanks()
@@ -3407,28 +3217,149 @@ local function bindFullscreenSwitcher()
 
 end
 
+local function initRenderCode()
+
+   pipeline:pushCode("poly_shape", [[
+    local col = {1, 0, 0, 1}
+    local inspect = require "inspect"
+
+    while true do
+        love.graphics.setColor(col)
+
+        local verts = graphic_command_channel:demand()
+        --local verts = graphic_command_channel:pop()
+        --print('verts', inspect(verts))
+        love.graphics.polygon('fill', verts)
+
+        coroutine.yield()
+    end
+    ]])
+
+   pipeline:pushCode('clear', [[
+    while true do
+        love.graphics.clear{0.5, 0.5, 0.5}
+        coroutine.yield()
+    end
+    ]])
+
+   pipeline:pushCode('set_transform', [[
+    local gr = love.graphics
+    local yield = coroutine.yield
+    while true do
+        gr.applyTransform(graphic_command_channel:demand())
+        yield()
+    end
+    ]])
+
+   pipeline:pushCode('pop_transform', [[
+    local gr = love.graphics
+    local yield = coroutine.yield
+    while true do
+        gr.origin()
+        yield()
+    end
+    ]])
+
+   pipeline:pushCode("poly_shape_smart", [[
+    local inspect = require 'inspect'
+    local col = {1, 0, 0, 1}
+
+    -- какое название лучше?
+    local hash = {}
+
+    local verts = nil
+    --local id = nil
+
+    while true do
+        love.graphics.setColor(col)
+
+        local id = graphic_command_channel:demand()
+        local cmd = graphic_command_channel:demand()
+
+        -- команды cmd:
+        -- new      - создать новый объект
+        -- draw     - рисовать существущий
+        -- ?????? draw_new - обновить координаты и рисовать ???????
+        -- remove   - удалить объект
+
+        if cmd == "new" then
+            verts = graphic_command_channel:demand()
+            hash[id] = verts
+        elseif cmd == "draw" then
+            verts = hash[id]
+        elseif cmd == "remove" then
+            hash[id] = nil
+        end
+
+        --print('id', id)
+        --print('cmd', cmd)
+        --print('verts', inspect(verts))
+
+        love.graphics.polygon('fill', verts)
+
+        coroutine.yield()
+    end
+    ]])
+
+end
+
+local function eachShape_smart(b, shape)
+
+
+
+   local shape_type = pw.polyShapeGetType(shape)
+
+   if shape_type == pw.CP_POLY_SHAPE then
 
 
 
 
 
+      local body_wrap = pw.cpBody2Body(b)
+      local tank = body_wrap.user_data
+
+      if not tank then
+         error("tank is nil")
+      end
+
+      pipeline:open('poly_shape_smart')
+      pipeline:push(tank.id)
+
+      if tank.first_render then
+         local verts = gather_verts(shape)
+         pipeline:push('new')
+         pipeline:push(verts)
 
 
 
 
 
+      else
 
+         local len = vecl.len(b.v.x, b.v.y)
+         local epsilon = 0.0001
+         if len < epsilon then
+            pipeline:push('draw')
 
+         else
 
+            local verts = gather_verts(shape)
+            pipeline:push('new')
+            pipeline:push(verts)
+            tank.first_render = true
+         end
 
+      end
 
+      pipeline:close()
+   end
 
+end
 
+local function initPhysIterators()
+   bodyIter = pw.newEachSpaceBodyIter(eachBody)
 
-
-
-function drawMiniMap()
-
+   shapeIter = pw.newEachBodyShapeIter(eachShape_smart)
 end
 
 local function init()
@@ -3436,7 +3367,12 @@ local function init()
    print('init started')
 
    metrics.init()
+
+   pw.init(pipeline)
+
    initJoy()
+   initRenderCode()
+   initPhysIterators()
 
 
 
@@ -3463,8 +3399,7 @@ local function init()
 
 
    arena = Arena.new("arena.lua")
-   terrain()
-   makeArmy()
+
    local corners = getTerrainCorners()
    if corners then
       for _, c in ipairs(corners) do
@@ -3477,8 +3412,6 @@ local function init()
 
 
 
-
-   pw.init(pipeline)
 
 
 
@@ -3616,22 +3549,73 @@ local State = {}
 local state = 'map'
 
 local function spawnTanks()
+   local options = {
+      body_opts = {
+         use_print = false,
+      },
+   }
+
    local tanks_num = 500
-   local minx, miny = -2000, -20000
-   local maxx, maxy = 20000, 20000
+
+   local minx, maxx = 0, 4000
+   local miny, maxy = 0, 4000
+
    for _ = 1, tanks_num do
       local px, py = rng:random(minx, maxx), rng:random(miny, maxy)
-      local tank = Tank.new(vec2(px, py))
+      local tank = Tank.new(vec2(px, py), options)
       table.insert(tanks, tank)
    end
 end
 
+
+local function cameraScale(j)
+   local axes = { j:getAxes() }
+   local dy = axes[2]
+   local factor = 0.01
+   if dy > 0 then
+      camera:scale(1 + factor, 1 + factor)
+   elseif dy < 0 then
+      camera:scale(1 - factor, 1 - factor)
+   end
+end
+
+
+local function cameraMovement(j)
+   local axes = { j:getAxes() }
+   local dx, dy = axes[4], axes[5]
+   local amount_x, amount_y = 10, 10
+   local tx, ty = 0, 0
+   local changed = false
+
+   if dx > 0 then
+      changed = true
+      tx = -amount_x
+   elseif dx < 0 then
+      changed = true
+      tx = amount_x
+   end
+
+   if dy > 0 then
+      changed = true
+      ty = -amount_y
+   elseif dy < 0 then
+      changed = true
+      ty = amount_y
+   end
+
+   if changed then
+      camera:translate(tx, ty)
+   end
+end
+
 local stateCoro = coroutine.create(function(dt)
+
    spawnTanks()
+
    while true do
       if state == 'map' then
          process_events()
-         render()
+         renderScene()
          updateTanks()
 
 
@@ -3642,6 +3626,9 @@ local stateCoro = coroutine.create(function(dt)
 
 
 
+
+         cameraScale(joy)
+         cameraMovement(joy)
 
          moveCamera()
 
