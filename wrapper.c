93de2fdc26f2071cd6ee858c9ff76c9a7cdabaa6
nagolove <dekar.own@gmail.com>
2022-02-19 17:40:42 +0300

    pass cdata as userdata(or lightuserdata)

--- wrapper.c
+++ wrapper.c
@@ -51,12 +51,32 @@ static int each_body(lua_State *lua) {
 
 static cpSpace *cur_space = NULL;
 
+typedef struct {
+    int i1, i2, i3;
+    double d4;
+    uint8_t b5;
+} Combo;
+
+static int pass_combo(lua_State *lua) {
+    Combo *combo = lua_topointer(lua, 1);
+    printf("combo.i1 %d\n", combo->i1);
+    printf("combo.i2 %d\n", combo->i2);
+    printf("combo.i3 %d\n", combo->i3);
+    printf("combo.d4 %f\n", combo->d4);
+    printf("combo.b5 %d\n", combo->b5);
+    return 0;
+}
+
 static int init_space(lua_State *lua) {
     int type = lua_type(lua, 1);
     printf("type %d\n", type);
-    /*cur_space = lua_touserdata(lua, 1);*/
-    cur_space = lua_topointer(lua, 1);
-    printf("cur_space %x\n", cur_space);
+
+    cur_space = lua_touserdata(lua, 1);
+    printf("cur_space (userdata)%p\n", cur_space);
+
+    cur_space = (cpSpace*)lua_topointer(lua, 1);
+    printf("cur_space (pointer)%p\n", cur_space);
+
     return 0;
 }
 
@@ -67,6 +87,7 @@ extern int luaopen_wrapper(lua_State *lua) {
          {"average", average},
          {"each_body", each_body},
          {"init_space", init_space},
+         {"pass_combo", pass_combo},
          {NULL, NULL}
     };
     /*luaL_newlib(lua, functions);*/
