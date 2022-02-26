deb6a639ea7ec2de98e2eb802f4549b5a18fa6f7
nagolove <dekar.own@gmail.com>
2022-02-26 23:00:16 +0300

    More functions moved to C.

--- wrapper.c
+++ wrapper.c
@@ -1,39 +1,38 @@
-#include "chipmunk.h"
+#include "chipmunk/chipmunk.h"
+#include "chipmunk/chipmunk_structs.h"
+#include <assert.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 #include <stdint.h>
 #include <stdio.h>
 
-/* The function we'll call from the lua script */
-static int average(lua_State *L)
-{
-	/* get number of arguments */
-	int n = lua_gettop(L);
-	double sum = 0;
-	int i;
-
-	/* loop through each argument */
-	for (i = 1; i <= n; i++)
-	{
-		if (!lua_isnumber(L, i)) 
-		{
-			lua_pushstring(L, "Incorrect argument to 'average'");
-			lua_error(L);
-		}
-
-		/* total the arguments */
-		sum += lua_tonumber(L, i);
-	}
-
-	/* push the average */
-	lua_pushnumber(L, sum / n);
-
-	/* push the sum */
-	lua_pushnumber(L, sum);
-
-	/* return the number of results */
-	return 2;
+static void stackDump (lua_State *L) {
+    int i;
+    int top = lua_gettop(L);
+    for (i = 1; i <= top; i++) { /* repeat for each level */
+        int t = lua_type(L, i);
+        switch (t) {
+            case LUA_TSTRING: { /* strings */
+                                  printf("’%s’", lua_tostring(L, i));
+                                  break;
+                              }
+            case LUA_TBOOLEAN: { /* booleans */
+                                   printf(lua_toboolean(L, i) ? "true" : "false");
+                                   break;
+                               }
+            case LUA_TNUMBER: { /* numbers */
+                                  printf("%g", lua_tonumber(L, i));
+                                  break;
+                              }
+            default: { /* other values */
+                         printf("%s", lua_typename(L, t));
+                         break;
+                     }
+        }
+        printf(" "); /* put a separator */
+    }
+    printf("\n"); /* end the listing */
 }
 
 static int each_body(lua_State *lua) {
@@ -51,31 +50,149 @@ static int each_body(lua_State *lua) {
 
 static cpSpace *cur_space = NULL;
 
-typedef struct {
-    int i1, i2, i3;
-    double d4;
-    uint8_t b5;
-} Combo;
-
-static int pass_combo(lua_State *lua) {
-    Combo *combo = lua_topointer(lua, 1);
-    printf("combo.i1 %d\n", combo->i1);
-    printf("combo.i2 %d\n", combo->i2);
-    printf("combo.i3 %d\n", combo->i3);
-    printf("combo.d4 %f\n", combo->d4);
-    printf("combo.b5 %d\n", combo->b5);
+static int init_space(lua_State *lua) {
+    /*int type = lua_type(lua, 1);*/
+    /*printf("type %d\n", type);*/
+    /*cur_space = (cpSpace*)lua_topointer(lua, 1);*/
+    cur_space = cpSpaceNew();
+    lua_pushlightuserdata(lua, cur_space);
     return 0;
 }
 
-static int init_space(lua_State *lua) {
-    int type = lua_type(lua, 1);
-    printf("type %d\n", type);
+static int free_space(lua_State *lua) {
+    luaL_checktype(lua, 1, LUA_TLIGHTUSERDATA);
+    cpSpace *space = (cpSpace*)lua_topointer(lua, 1);
+    cpSpaceFree(space);
+}
 
-    cur_space = lua_touserdata(lua, 1);
-    printf("cur_space (userdata)%p\n", cur_space);
+#define DENSITY (1.0/10000.0)
 
-    cur_space = (cpSpace*)lua_topointer(lua, 1);
-    printf("cur_space (pointer)%p\n", cur_space);
+static int new_box_body(lua_State *lua) {
+    // in: ширина, высота, таблица с инфой
+
+    int top = lua_gettop(lua);
+    if (top != 3) {
+        lua_pushstring(lua, "Function should receive only 3 arguments.\n");
+        lua_error(lua);
+    }
+
+    luaL_checktype(lua, 1, LUA_TNUMBER);
+    luaL_checktype(lua, 2, LUA_TNUMBER);
+    luaL_checktype(lua, 3, LUA_TTABLE);
+
+    int w = (int)lua_tonumber(lua, 1);
+    int h = (int)lua_tonumber(lua, 2);
+
+    cpFloat mass = w * h * DENSITY;
+    cpFloat moment = cpMomentForBox(mass, w, h);
+    cpBody *b = cpBodyNew(mass, moment);
+
+    if (!cur_space) {
+        lua_pushstring(lua, "Space pointer is null.\n");
+        lua_error(lua);
+    }
+
+    cpSpaceAddBody(cur_space, b);
+
+    // ссылка на табличку, связанную с телом
+    int reg_index = luaL_ref(lua, LUA_REGISTRYINDEX);
+    b->userData = (void*)reg_index;
+
+    lua_pushlightuserdata(lua, b);
+
+    return 1;
+}
+
+void on_each_body(cpBody *body, void *data) {
+    printf("on_each_body\n");
+}
+
+void print_space_info(cpSpace *space) {
+    printf("iterations %d\n", space->iterations);
+    printf("damping %f\n", space->damping);
+    printf("data %p\n", space->userData);
+    printf("curr_dt %f\n", space->curr_dt);
+    printf("stamp %d\n", space->stamp);
+}
+
+static int query_all_shapes(lua_State *lua) {
+    printf("cur_space %p\n", cur_space);
+    print_space_info(cur_space);
+    assert(cur_space);
+
+    printf("C: query_all_shapes\n");
+
+    luaL_checktype(lua, 1, LUA_TFUNCTION);
+
+    cpSpaceEachBody(cur_space, on_each_body, NULL);
+    /*cpSpaceEachBody(cur_space, NULL, NULL);*/
+
+    printf("C: query_all_shapes after\n");
+    /*printf("C: query_all_shapes after\n");*/
+
+    return 0;
+}
+
+static int step(lua_State *lua) {
+    luaL_checktype(lua, 1, LUA_TNUMBER);
+
+    if (!cur_space) {
+        lua_pushstring(lua, "Space pointer is null.\n");
+        lua_error(lua);
+    }
+
+    double dt = lua_tonumber(lua, 1);
+    cpSpaceStep(cur_space, dt);
+
+    return 0;
+}
+
+static int set_position(lua_State *lua) {
+    luaL_checktype(lua, 1, LUA_TLIGHTUSERDATA);
+
+    int top = lua_gettop(lua);
+    if (top != 1) {
+        lua_pushstring(lua, "Function expect 1 argument.\n");
+        lua_error(lua);
+    }
+
+    cpBody *b = (cpBody*)lua_topointer(lua, 1);
+    /*b->p.x = lua_tonumber(lua, 1);*/
+    /*b->p.y = lua_tonumber(lua, 2);*/
+    double x = lua_tonumber(lua, 1);
+    double y = lua_tonumber(lua, 2);
+    cpVect pos = { .x = x, .y = y};
+    cpBodySetPosition(b, pos);
+    /*cpBodySetPosition(b, { .x = x, .y = y });*/
+    
+    return 0;
+}
+
+static int apply_impulse(lua_State *lua) {
+    luaL_checktype(lua, 1, LUA_TLIGHTUSERDATA);
+    luaL_checktype(lua, 2, LUA_TNUMBER);
+    luaL_checktype(lua, 3, LUA_TNUMBER);
+    luaL_checktype(lua, 4, LUA_TNUMBER);
+    luaL_checktype(lua, 5, LUA_TNUMBER);
+
+    cpVect impulse = { 
+        .x = lua_tonumber(lua, 2),
+        .y = lua_tonumber(lua, 3),
+    };
+
+    cpVect point = { 
+        .x = lua_tonumber(lua, 4),
+        .y = lua_tonumber(lua, 5),
+    };
+
+    int top = lua_gettop(lua);
+    if (top != 5) {
+        lua_pushstring(lua, "Function expect 5 argument.\n");
+        lua_error(lua);
+    }
+
+    cpBody *b = (cpBody*)lua_topointer(lua, 1);
+    cpBodyApplyImpulseAtLocalPoint(b, impulse, point);
 
     return 0;
 }
@@ -84,10 +201,16 @@ extern int luaopen_wrapper(lua_State *lua) {
     /*cpSpace *space = cpSpaceNew();*/
     static const struct luaL_Reg functions[] =
     {
-         {"average", average},
-         {"each_body", each_body},
          {"init_space", init_space},
-         {"pass_combo", pass_combo},
+         {"free_space", free_space},
+         {"step", step},
+
+         {"query_all_shapes", query_all_shapes},
+
+         {"new_box_body", new_box_body},
+         {"set_position", set_position},
+         {"apply_impulse", apply_impulse},
+
          {NULL, NULL}
     };
     /*luaL_newlib(lua, functions);*/
