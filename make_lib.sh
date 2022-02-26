80cf4f3e8849a18d9720814c8e630dd1efa2a268
nagolove <dekar.own@gmail.com>
2022-02-27 01:23:32 +0300

    linking from cwd.

--- make_lib.sh
+++ make_lib.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 #gcc -shared -o libwrapper.so wrapper.c \
-gcc -shared -o wrapper.so wrapper.c \
+    #-L ~/projects/Chipmunk2D/src \
+    #-L ../../ \
+gcc -shared -o wrp.so wrapper.c \
     -g3 \
     -std=c99 \
     -I ~/projects/Chipmunk2D/include/ \
     -I /usr/include/lua5.1/ \
-    -L ~/projects/Chipmunk2D/src \
+    -L . \
     -fPIC \
     -lchipmunk \
     -lluajit-5.1 
