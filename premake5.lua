6787d029d9ce78dc696e7d542b1fd67056729c21
nagolove <dekar.own@gmail.com>
2022-05-21 00:18:18 +0300

    new module.

--- premake5.lua
+++ premake5.lua
@@ -1,43 +1,45 @@
 workspace "wrapper"
     configurations { "Debug", "Release" }
 
-    project "wrp"
-        kind "SharedLib"
-        language "C"
-        
-        --targetdir "bin/%{cfg.buildcfg}"
-        --targetdir "bin/%{cfg.buildcfg}"
-        
-        targetprefix ""
-        targetdir "."
+    includedirs { 
+        "/usr/include/luajit-2.1",
+        --"~/projects/Chipmunk2D/include"
+        "/home/nagolove/projects/Chipmunk2D/include",
+        "/home/nagolove/myprojects/lua_capi",
+    }
+    buildoptions { 
+        "-fPIC",
+        "-Wall",
+        "-Werror",
+        "-Wno-strict-aliasing",
+        --"-Wno-unused-function",
+    }
+    links { 
+        --"luajit-5.1", 
+        "lua5.1",
+        "chipmunk",
+        "mem_guard",
+        "lua_tools",
+    }
+    libdirs { 
+        "/home/nagolove/projects/Chipmunk2D/src/",
+        "/home/nagolove/myprojects/c_guard",
+        "/home/nagolove/myprojects/lua_capi",
+    }
+    language "C"
+    kind "SharedLib"
+    --targetdir "bin/%{cfg.buildcfg}"
+    --targetdir "bin/%{cfg.buildcfg}"
+    targetprefix ""
+    targetdir "."
 
+    project "wrp"
         files { "**.h", "wrapper.c" }
-        includedirs { 
-            "/usr/include/luajit-2.1",
-            --"~/projects/Chipmunk2D/include"
-            "/home/nagolove/projects/Chipmunk2D/include",
-            "/home/nagolove/myprojects/lua_capi",
-        }
-        buildoptions { 
-            "-fPIC",
-            "-Wall",
-            "-Werror",
-            "-Wno-strict-aliasing",
-            --"-Wno-unused-function",
-        }
-        links { 
-            --"luajit-5.1", 
-            "lua5.1",
-            "chipmunk",
-            "mem_guard",
-            "lua_tools",
-        }
-        libdirs { 
-            "/home/nagolove/projects/Chipmunk2D/src/",
-            "/home/nagolove/myprojects/c_guard",
-            "/home/nagolove/myprojects/lua_capi",
-        }
         --links { "lua5.1" }
+    project "diamond_and_square"
+        files { "**.h", "diamond_and_square.c" }
+        --links { "lua5.1" }
+
 
     filter "configurations:Debug"
     defines { "DEBUG" }
